1832567719c26da39986a1523dfe6d1e988a1f7b
cross1usf <jakeedcross@gmail.com>
2022-07-04 19:36:01 -0400

    feat - testing spring HATEOAS

---
+++ spring-hateoas/src/main/java/com/example/springhateoas/SpringHateoasImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHateoasImplementationApplication.class, args);
+	}
+
+}
